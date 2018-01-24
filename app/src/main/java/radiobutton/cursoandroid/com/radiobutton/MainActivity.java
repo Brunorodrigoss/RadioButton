5b7e950ea9991fb05929e37d3eb6b3841e1eb916
Bruno Rodrigo <bnorodrigo@gmail.com>
2018-01-24 10:43:25 -0200

    Ajustes para o correto funcionamento do RadioGroup

--- app/src/main/java/radiobutton/cursoandroid/com/radiobutton/MainActivity.java
+++ app/src/main/java/radiobutton/cursoandroid/com/radiobutton/MainActivity.java
@@ -1,44 +1,37 @@
 package radiobutton.cursoandroid.com.radiobutton;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    private RadioButton radioButton1;
-    private RadioButton radioButton2;
-    private RadioButton radioButton3;
-    private Button button;
-    private TextView textView;
+    private RadioGroup radioGroup;
+    private RadioButton radioButtonEscolhido;
+    private Button botaoEscolher;
+    private TextView resultadoApresentado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        radioButton1 = findViewById(R.id.radio1);
-        radioButton2 = findViewById(R.id.radio2);
-        radioButton3 = findViewById(R.id.radio3);
+        radioGroup = findViewById(R.id.radioGroupId);
+        botaoEscolher = findViewById(R.id.button_id);
+        resultadoApresentado = findViewById(R.id.resutadoId);
 
-        button = findViewById(R.id.button_id);
-        textView = findViewById(R.id.resutadoId);
-
-        button.setOnClickListener(new View.OnClickListener() {
+        botaoEscolher.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (radioButton1.isChecked()){
-                    textView.setText("Você torce para o "+radioButton1.getText()+"!");
+                int idRadioButtonEscolhido = radioGroup.getCheckedRadioButtonId();
 
-                } else if(radioButton2.isChecked()){
-                    textView.setText("Você torce para o "+radioButton2.getText()+"!");
-
-                } else if(radioButton3.isChecked()){
-                    textView.setText("Você torce para o "+radioButton3.getText()+"!");
+                if (idRadioButtonEscolhido > 0) {
+                    radioButtonEscolhido = (RadioButton) findViewById(idRadioButtonEscolhido);
+                    resultadoApresentado.setText("Você torce para o "+radioButtonEscolhido.getText()+"!");
                 }
             }
         });
